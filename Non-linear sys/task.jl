c7e82c091a10adf739350f729d41652e5517bd12
Wasserschmitt <iushin.kv@phystech.edu>
2022-11-15 13:46:09 +0300

    Non linear eqs and sys are added

--- Non-linear sys/task.jl
+++ Non-linear sys/task.jl
@@ -26,7 +26,6 @@ function newtonsys(f, x, J, temp; maxiter=50, xtol=1e-6, ftol=1e-6)
         y .= f(x, temp)
         δx .= .- (J(x, temp) \ y)
         x .+= δx
-	print(x)
         norm(δx) < xtol && return x
         norm(y) < ftol && return x
     end
@@ -37,14 +36,14 @@ end
 #
 
 
+x = [0.5, 2]
+temps = [0.85, 0.87, 0.89, 0.91, 0.93, 0.95, 0.97, 0.98]
+
 file = open("dump.txt", "w")
 
-for i in 1:8
-    x = [0.5, 2]
-    cur_temp = 0.85+0.2*(i-1)
-    res = newtonsys(f1, x, J1, cur_temp)
-    pushfirst!(res, cur_temp)
-    println(file, join(res, ' '))
+for temp in temps
+    res = newtonsys(f1, x, J1, temp)
+    println(file, join([temp, res[1], res[2], 8*temp/(3*res[1]-1) - 3/(res[1]^2), 8*temp/(3*res[2]-1) - 3/(res[2]^2)], ' '))
 end
 close(file)
 
